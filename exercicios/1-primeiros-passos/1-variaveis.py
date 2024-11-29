832c021460bec01f4f6c582e0ee957b2db977055
Ariel Piloto <165099410+arielpiloto@users.noreply.github.com>
2024-11-29 21:34:07 +0000

    Resolução de exercícios 2

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Ariel'
+sobrenome = 'Piloto'
+print(nome, sobrenome)
 
 # Declare variáveis de cada tipo numérico
-
+idade = 33
+altura = 177
+altura_float = 177.30
+print(idade, altura, altura_float)
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
